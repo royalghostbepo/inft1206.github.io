e3fd3a19cf0ff1e23f6a6dacbeb44e034df26e9c
Joseph <bepojoseph9@gmail.com>
2023-11-18 22:04:09 -0500

    renamed my js files as script copying code from part 3 to 4, since it is a continuatio.

--- lab4/part3/script.js
+++ lab4/part3/script.js
@@ -2,7 +2,6 @@
 
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
-
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
